5550c7f26c54bc78210116385e0bdfd7edf43ad1
sharuka-jay <65723319+sharuka-jay@users.noreply.github.com>
2023-01-19 09:36:17 +0530

    Update Home2Application.java
    33

--- src/main/java/com/example/home2/Home2Application.java
+++ src/main/java/com/example/home2/Home2Application.java
@@ -1,13 +1,25 @@
-package com.example.home2;
+package com.example.testingpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Home2Application {
+@RestController
+public class TestingproApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Home2Application.class, args);
+        SpringApplication.run(TestingproApplication.class, args);
     }
 
+    @GetMapping("/")
+    public String home() {
+        return "welcome home";
+    }
+
+    @GetMapping("/dash")
+    public String dash() {
+        return "welcome dash";
+    }
 }
