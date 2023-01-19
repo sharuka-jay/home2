33fd4d2dc23ef986099637504259c5733369f934
sharukaJ <sharuka.j@affiniti.biz>
2023-01-19 09:28:08 +0530

    22

---
+++ src/main/java/com/example/home2/Home2Application.java
@@ -0,0 +1,13 @@
+package com.example.home2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Home2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Home2Application.class, args);
+    }
+
+}
