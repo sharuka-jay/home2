320681ba79a679e1569e2db03644a6c9a9ae40ed
sharuka-jay <65723319+sharuka-jay@users.noreply.github.com>
2023-01-19 09:24:13 +0530

    Add files via upload
    1

---
+++ src/test/java/com/example/home2/Home2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Home2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
